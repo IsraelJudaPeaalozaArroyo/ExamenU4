4683c215fd7c7f56094794b80490d22dd6fedd34
Israel Juda Peñaloza Arroyo <71736208+IsraelJudaPeaalozaArroyo@users.noreply.github.com>
2020-12-15 18:10:57 -0600

    Create NodoArbol.java

---
+++ NodoArbol.java
@@ -0,0 +1,14 @@
+package examen.unidad.pkg4;
+public class NodoArbol {
+    int dato;
+    
+    NodoArbol hijoIzquierdo, hijoDerecho;
+    public NodoArbol (int d){
+        this.dato=d;
+        
+        this.hijoDerecho= null;
+        this.hijoIzquierdo=null;
+    }
+    
+    
+}
